2c29684c27cfad9896b4bae24b1a6ac6f61a160d
Oleksandr Onopchenko <a3atlaslife@gmail.com>
2023-12-31 15:57:01 +0200

    feat: pubkey

---
+++ acquiring/pubkey.go
@@ -0,0 +1,25 @@
+package acquiring
+
+import (
+	"context"
+	"github.com/fairytale5571/go-mono/internal/api"
+)
+
+// PubKeyResponse відповідь на запит публічного ключа
+type PubKeyResponse struct {
+	Key string `json:"key"`
+}
+
+// PubKey отримання публічного ключа
+func (a *Acquiring) PubKey(ctx context.Context) (*PubKeyResponse, error) {
+	var response PubKeyResponse
+	err := a.apiClient.SendRequest(ctx, api.Request{
+		Method:   "GET",
+		Path:     "/pubkey",
+		Response: &response,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &response, nil
+}
